7414959844b4f67ca9ff53aca0813bb72a22f2b4
Dzhennet Berdieva <berdievajennet@yandex.ru>
2024-04-17 07:56:08 +0300

    changes have been made to the game code preme

--- games/prime.js
+++ games/prime.js
@@ -3,10 +3,15 @@ import logicOfGame from '../src/index.js';
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
+  if (num < 2) {
+    return false;
   }
-  return num !== 1;
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const generateQuestionAndAnswer = () => {
